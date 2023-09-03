ecece5416df88ff41c841d92b39a2d8c088ff8fb
Noman Nejrabi <nejrabi.n@gmail.com>
2023-09-03 12:31:05 +0430

    Add html css and js codes

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//What number buttons are pressed
+var numButton = document.querySelectorAll(".btn");
+var showNum = document.querySelector(".screen");
+
+
+numButton.forEach(function(button){
+  button.addEventListener('click', function(event){
+    if(event.target.innerHTML == "c"){
+      return showNum.value = "";
+    } else if (event.target.innerHTML == "=") {
+      return;
+    }
+    let view = event.target.dataset.num;
+    showNum.value += view;
+  });
+});
+
+//When equal is pressed
+var equalButton = document.querySelector(".btn-equal")
+equalButton.addEventListener('click', function(event){
+  if(showNum.value == ""){
+    return alert("Please Enter a Value");
+  }
+  showNum.value = showNum.value + "=" + eval(showNum.value);
+});
